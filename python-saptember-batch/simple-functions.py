ac2cca1bd2f3098a499b5628dbe4d348ff3914b0
Naresh Kumar <banvro07@gmail.com>
2023-09-06 02:05:12 -0700

    python simple funtions

---
+++ python-saptember-batch/simple-functions.py
@@ -0,0 +1,40 @@
+# functions : 
+# 1) print()   : to print anything on conole
+# 2) input()  : to get input from user
+# 3) len()  : return len 
+# 4) type()  : to know about dataypes
+
+# a = 100
+
+# print("this i smeeea", a, "hello")
+
+# a = 10
+# b = 20
+
+# c = a + b
+# print(c)
+
+# concatenation
+
+# num1 = int(input("Enter first number : "))
+# num2 = int(input("Enter Second Number : "))
+
+# zx = num1 + num2
+
+# print("The sum is : ", zx)
+
+
+# len()
+
+# a = "12012817297"
+
+# print(len(a))
+
+# type()
+
+# a = (12, 10)
+
+# print(type(a))
+
+
+
