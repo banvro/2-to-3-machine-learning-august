6509e908d8cad33c8b544c7caf434b79da4f6eff
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-07 02:07:49 -0700

    Create elif-and-question.py

---
+++ elif-and-question.py
@@ -0,0 +1,90 @@
+# age = int(input("Enter your age : "))
+# # 19 > 18
+# if age > 19:
+#     print("You are eligible for voat")
+    
+# elif age == 18:
+#     print("welcome this is you first voat..")
+
+# elif age == 19:
+#     print("you can do t...")
+
+# else:
+#     print("you are not eligibla")
+
+
+
+
+
+# +
+# -
+# /
+# *
+# %
+
+
+# inpput()  13
+
+# even 
+
+
+# odd
+
+# number = int(input("enter your number :  "))
+
+# if number % 2 == 0:
+#     print("it is even")
+
+# else:
+#     print("it is odd")
+
+
+
+
+ *************  Welcom to Coalculater  *************
+ 
+ num1 = 
+ 
+ num2 = 
+ 
+        Choose any one
+1) Adation
+2) Subtraction
+3) Multiplication
+4) Devsion
+
+
+choice = 1
+
+if choice == 1:
+    zx = num1 + num2
+    print("the sum is : ", zx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
