27288a9acb93dd65359f05e1d062c717f8a150e4
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-08 01:55:29 -0700

    Create start-loops.py

---
+++ python-saptember-batch/start-loops.py
@@ -0,0 +1,31 @@
+a = "kuagsd"
+
+
+# for i in a:
+#     print(i)
+#     print("hlo")
+#     print(i, i, i)
+#     print("xyz")
+
+# range(start, end, increament)
+
+# default : 
+#     start = 0
+#     end = n - 1
+#     increment = 1
+
+
+# for i in range(2, 10, 2):
+#     print(i)
+
+
+# print("asbdjasdbj", a, "ajsbdkja")
+
+
+
+
+
+
+
+
+    
