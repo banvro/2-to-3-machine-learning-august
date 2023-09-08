f54378694e809091773cab3dd1b3b57c3dc7a5f7
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-08 02:32:40 -0700

    Update start-loops.py

--- python-saptember-batch/start-loops.py
+++ python-saptember-batch/start-loops.py
@@ -26,6 +26,45 @@ a = "kuagsd"
 
 
 
+# 9 x 1 = 9
+# 9 x 2 = 18
+# .\
+# ...
+# .
 
+# .
+# ..
+
+# 9 x 10  90
+
+# for i in range (1,11, 1):
+#     print ("9 * " , i, "=" , i * 9)
+
+#  9 x 1 = 9
+# for i in range(1, 11):
+#     print("9 x", i, "=", 9 * i)
+
+# a  = 182391862
+    
+# type casting : 
+
+# a = 10
+# print(a, type(a))
+
+# b = float(a)
+# print(b, type(b))
+
+# a = [2, 3, 5, 12, 10, 9]
+
+# tol = 0
+
+# for i in a:
+#     tol = tol + i
+# print(tol)
+
+a = 1972981263
+    
+    
+    
 
     
