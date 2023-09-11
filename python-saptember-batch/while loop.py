239db0aa948e354ccf23e210672a3dbe8c4f68eb
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-11 03:06:25 -0700

    Create while loop.py

---
+++ python-saptember-batch/while loop.py
@@ -0,0 +1,102 @@
+# For Loop :
+
+# a = [2, 3,4, 6]
+
+# for i in a:
+#     print(i)
+
+# While Loop : 
+
+# a = 10
+# # 90 < 100
+# while a < 100:
+#     a = a + 10
+#     print("okkk.", a)
+
+# a = 10
+# while True:
+#     if a < 100:
+#         a = a + 10
+#         print("okkk.", a)
+
+
+
+
+# break
+# continue
+# pass
+
+
+# for i in range(1, 10):
+#     if i == 5:
+#         # break
+#         # continue
+#         pass
+#     print(i)
+
+
+
+
+
+# while True:
+#     uinp = input("Press q for quit and s for start : ")
+
+#     if uinp == "q":
+#         break
+#     elif uinp == "s":
+#         print("""
+#                 kjdgkasjdbkajdbk
+#               sdfsdlfndskf
+#               sdfnsdfsldfnk
+#               skdfkj    """)
+#     else:
+#         print("enter something vaild")
+
+
+
+# assert 10 == 9, "you are wrong.."
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+# a12 = 10
+
+# my_name = "hlo"
+
+
+# keywords : int, list, print, input 
+
+# int = 20
+
+
+
+
+# comments : use to increse code readabilty
+
+# sdhfsdhfshdkf sdkfbskdjfbks
+
+"""
+    zkjbckzc
+    zxkjbckjzxcbzxc
+    zxkcjbkxcbz
+    zxcbzkxjcbzkc
+"""
+# a = 0
+# while a < 34:
+    
+#     b = a + 1
+#     print(b)
+
+a = 0
+b = 1
+
+while a < 35:
+    print(a)
+    c = a + b
+    a = b
+    b = c
+
+    
+
+
+
+
