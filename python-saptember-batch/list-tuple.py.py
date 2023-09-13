617518428fa0f31cbcd160751e270db93e2510f1
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-12 21:15:54 -0700

    Add files via upload

---
+++ python-saptember-batch/list-tuple.py.py
@@ -0,0 +1,104 @@
+# # List : 
+
+# # list
+# # tuple
+# # set
+# # dictionry
+
+
+# # its is used to collcat multipal data in a single variable...
+
+# # List :  ordered, mutable, allow duplicasy
+# # []
+
+
+# # lst = [12, 100.8, "hello", 100, 80, 67]
+
+# # print(lst, type(lst))
+
+# # index : 
+
+# # print(lst[-3] + lst[0])
+
+# # slicing   :
+
+# # print(lst[1 : 4])
+
+
+# # mutable..... changeable
+
+# lst = [12, 100.8, "hello", 100, 80, 67]
+
+# # # 1) append()
+
+# # # 2) insert()
+
+# # # lst.append(2000)
+# # lst.insert(1, 2000)
+
+# # print(lst)
+
+
+
+# # to delete data from list
+
+# # lst = [12, 100.8, "hello", 100, 80, 67, 100.8]
+
+# # 1) pop()
+
+# # 2) remove()
+
+
+# # lst.pop()
+# # lst.remove(100.8)
+
+# # # lst.clear()
+
+# # print(lst)
+
+
+# # lst = [12, 100.8, "hello", 100, 80, 67, 10]
+
+
+# # for i in lst:
+# #     print(i)
+
+
+
+
+
+# # Tuple : ordered, imutable, allow duplicasy
+# # ()
+
+
+
+# # tpl = (12, 100, "hlo", 800, 100)
+
+# # # print(tpl, type(tpl))
+
+# # print(tpl[2])
+
+# # lst = [1, 2, 3, 5]
+# # tpl = tuple(lst)
+
+# # print(tpl)
+
+
+# tpl = ("tree", "veg", "fruit", "can")
+
+
+# # list
+
+# ["tree", "veg", "fruit", "can"]
+
+
+
+
+
+
+
+
+
+
+
+
