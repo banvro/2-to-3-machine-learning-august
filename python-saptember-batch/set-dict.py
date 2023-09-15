89ba8560319bbe1bbd7cf5abc273774f5fd281ba
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-15 05:37:11 -0700

    Create set-dict.py

---
+++ python-saptember-batch/set-dict.py
@@ -0,0 +1,59 @@
+# List : ordered, allow duplicasy, mutable
+# Tuple : ordered, allow duplicasy, imutable
+
+# Set : unordered, do not allow dupliate values, mutable
+# {}
+
+# st = {12, 90, 100, "Hello", "moris", 100, 90, "hey"}
+
+# # st.add(2000)
+
+# # st.pop()
+# st.remove(100)
+# print(st)
+
+
+
+# dictionry : ordered, do not allow duplicasy, mutable
+# {}
+# {key : value, key : value}
+
+# x = {"name" : "kriss", "age" : 20, "number" : 92638734}
+
+# # print(x['number'])
+
+# x["Address"] = "this is my address"
+
+# x.pop("age")
+# print(x)
+# print(x, type(x))
+
+
+
+# dict = {
+#     1 : {
+#         "Name" : "Kriss",
+#         "Age" : 20,
+#         "Number" : 982364324
+#     },
+#     2 : {
+#         "Name" : "Moris",
+#         "Age" : 30,
+#         "Number" : 92813189236
+#     },
+# }
+# dict[3]["Name"] = "this is my address"
+# print(dict)
+
+
+# a = ["Name", "Age", "Number"]
+# b = ["Kriss", 20, 9823896434]
+
+# output
+# z = {"name" : "Kriss", "Age" : 20, "Number" : 982386274}
+
+# keysare = z.values()
+# print(keysare)
+# for i in keysare:
+#     print(i)
+
