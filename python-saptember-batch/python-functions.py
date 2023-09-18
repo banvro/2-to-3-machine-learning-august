41f2b5cf77bb43e7bbfb0770f76290865b8a821b
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-18 06:03:34 -0700

    Create python-functions.py

---
+++ python-saptember-batch/python-functions.py
@@ -0,0 +1,137 @@
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+
+
+# a = 30
+# b = 90
+# z = a + b
+# print(z)
+
+
+# a = 100
+# b = 200
+# c = a + b
+# print(c)
+
+
+
+# python functions : code optimization
+
+# declearation of function 
+
+# def xyz():
+#     print("i am good function...")
+
+# # call a function 
+
+# xyz()
+# xyz()
+# xyz()
+
+
+# DRY 
+
+# Perameters
+# Arguments
+
+
+# # perameters
+# def sumthis(a, b):
+#     c = a + b
+#     print(c)
+
+# # arguments
+# sumthis(100, 200)
+
+# sumthis(2, 10)
+
+
+# def savedata(a, b):
+#     print(f"user ka name is : {a} and user age is : {b}")
+
+# savedata(b = 20, a = "kriss")
+
+# default perameters
+
+# def cyz(x = 10, y = 20):
+#     c = x + y
+#     print(c)
+
+# cyz(1, 2)
+
+  
+# 1) *args   arbitrary postinal arguments
+# 2) **kwargs    keywords arbitrary postinnal arguments
+
+# def sumthis(*z):
+#     q = 0
+#     for i in z:
+#         q = q + i
+#     print(q)
+
+# sumthis(12, 71672187)
+
+
+
+# def xyz(**zx):
+#     for i in zx:
+#         print(i, zx[i])
+
+# xyz(name = "krss", age = 100)
+
+
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# .
+# .
+# .
+# .
+# .
+# 2 x 10 = 20
+
+
+# table(7)
+
+
+
+
+# def a(x):
+    
+#     y = 1
+#     while y<=10:
+#         z = x * y
+        
+#         print(x,"*",y,"=",z)
+#         y = y + 1
+
+# x = int(input("enter number"))
+
+# a(x)
+
+a = "qowehqoweqe293492834sdlknksdfnkdsf"
+
+
+print(a[-5:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
