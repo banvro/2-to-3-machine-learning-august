3c897e54586d9ef26fdfb6428211d94a0e86b271
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-19 02:24:02 -0700

    Create lambda-expression.py

---
+++ python-saptember-batch/lambda-expression.py
@@ -0,0 +1,20 @@
+# lambda expression  
+# lambda function
+# map()
+# filter()
+# reduce()
+
+
+# lambda arguments : expression
+
+# zx = lambda a, b : a + b
+
+# print(zx(100, 200))
+
+
+# py = lambda a : a ** 2
+
+# print(py(2))
+
+
+
