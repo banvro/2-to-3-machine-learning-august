53ea5d0fdfccfabf011e698058fd86adf58ef78d
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-09-19 02:09:26 -0700

    Create start-oops.py

---
+++ python-saptember-batch/start-oops.py
@@ -0,0 +1,96 @@
+# OOps:
+#     object oriented programming 
+    
+    # object classs
+    
+# class => is a bluprint of an object
+# object = instance of a class
+# self = for refrenceing
+
+# class mycalss:
+#     def car(self):
+#         print("this is a car")
+    
+#     def bus(self):
+#         print("this is bus")
+        
+# xyz = mycalss()
+# xyz.bus(12)
+        
+# xyz.car()
+
+# constructer 
+# attrebutes
+
+# class cls1:
+#     def mthd1(self):
+#         print("hello world")
+    
+#     def mthd2(self, a, b):
+#         c = a + b
+#         print(c)
+    
+#     def mthd3(self):
+#         print("nooooooo")
+
+# obj = cls1()
+# obj.mthd2(100, 200)
+# obj.mthd3()
+
+
+
+# __name__
+# __initt__
+
+# constructer : is useed to initilize attrebutes and it calls automaticlly when we are create object of a class
+
+
+# class mycls:
+#     def __init__(self, a, b):
+#         self.name = a
+#         self.age = b
+    
+#     def car(self):
+#         print(f"i am a car {self.name}")
+
+# obj = mycls("kriss", 20)
+# obj.car()
+
+# obj1 = mycls("moris", 10)
+# obj1.car()
+
+
+# class
+# methods
+# object
+# self
+# constructer
+# attrebutes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
