8eec1c7507fb596e39788a4f5e565ebe97b501ec
Naresh <banvro07@gmail.com>
2024-07-15 14:48:50 +0530

    model

--- july web devlopment/Hello/hlo/models.py
+++ july web devlopment/Hello/hlo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactUs(models.Model):
+    fullname = models.CharField(max_length=150)
+    email = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+    message = models.TextField()
+
+
+# python manage.py makemigrations
+# python manage.py migrate
